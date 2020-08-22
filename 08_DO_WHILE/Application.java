d6d5947722af724b175304b332c866e41bc3635e
shubhamkamble <kshub44kamble@gmail.com>
2020-08-22 14:19:07 +0530

    added: do while

---
+++ 08_DO_WHILE/Application.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Application {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int value = 0;
+		do{
+			System.out.println("Enter a number: ");
+			value = scanner.nextInt();
+		}while(value != 5);
+
+		System.out.println("You Got it, number is 5.");
+	}
+}
